e9b77e34ab4424fe8d5000bed1b2446535f9cba5
ShauryaMagar <shaurya.magar1@gmail.com>
2024-09-29 07:57:53 -0700

    Win condition added

---
+++ Assets/PlayerCollisionj.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerCollisionj : MonoBehaviour
+{
+    public TextMeshProUGUI winText; // Reference to the "You Win!" Text object
+    public string targetTag = "TargetTag"; // Tag of the object that triggers the win condition
+
+    // This method will be called when the player enters a trigger zone
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the player collided with the object that has the specified tag
+        if (other.CompareTag(targetTag))
+        {
+            // Display the "You Win" message
+            winText.gameObject.SetActive(true);
+        }
+    }
+}
