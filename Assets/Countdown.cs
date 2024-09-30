99dc80cd6a04e349f0b3fbeacdc2699b10bcae3a
ShauryaMagar <shaurya.magar1@gmail.com>
2024-09-30 15:42:13 -0700

    Replay button added with functionality

--- Assets/Countdown.cs
+++ Assets/Countdown.cs
@@ -3,26 +3,36 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Countdown : MonoBehaviour
 {
     // Start is called before the first frame update
     float currentTime= 0f;
-    float startTime= 10f;
+    float startTime= 20f;
     [SerializeField] TextMeshProUGUI countdownText;
+    public Button ReplayButton;
     void Start()
     {
+        ReplayButton.gameObject.SetActive(false);
         currentTime = startTime;
+        StartCoroutine(StartCountdown());
     }
 
     // Update is called once per frame
-    void Update()
+    IEnumerator StartCountdown()
     {
-        if (currentTime > 0)
+        while (currentTime > 0f)
         {
-            currentTime -= 1 * Time.deltaTime;
-            countdownText.text = "Time Remaining: " +currentTime.ToString("0");
+            countdownText.text = "Time Remaining: " + currentTime.ToString("0");
+            yield return new WaitForSeconds(1f);
+            currentTime -= 1f;
         }
-        
-     }
+        countdownText.text = "Times Up";
+        ReplayButton.gameObject.SetActive(true);
+    }
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
--- Assets/PlayerCollision.cs
+++ Assets/PlayerCollision.cs
@@ -5,16 +5,14 @@ using TMPro;
 
 public class PlayerCollision : MonoBehaviour
 {
-    public TextMeshProUGUI winText; // Reference to the "You Win!" Text object
-    public string targetTag = "TargetTag"; // Tag of the object that triggers the win condition
+    public TextMeshProUGUI winText; 
+    public string targetTag = "TargetTag";
+    public string obstacleTag = "ObstacleTag";
 
-    // This method will be called when the player enters a trigger zone
     void OnTriggerEnter(Collider other)
     {
-        // Check if the player collided with the object that has the specified tag
         if (other.CompareTag(targetTag))
         {
-            // Display the "You Win" message
             winText.gameObject.SetActive(true);
         }
     }
